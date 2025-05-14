2d44e700cfb70538c7767180445fb4655f2e3cb2
= <steve.riebe@gmail.com>
2025-05-14 15:33:47 -0500

    Rename local variable, add calls to service layer for remaining items.

--- CartonCapsService/Controllers/CartonCapsReferralController.cs
+++ CartonCapsService/Controllers/CartonCapsReferralController.cs
@@ -11,39 +11,39 @@ namespace CartonCapsService.Controllers
     {
 
         private readonly ILogger<CartonCapsReferralController> _logger;
-        private readonly IUserReferallService _userProfileService;
+        private readonly IUserReferallService _userReferralService;
 
         public CartonCapsReferralController(ILogger<CartonCapsReferralController> logger, IUserReferallService userProfileService)
         {
             _logger = logger;
-            _userProfileService = userProfileService;
+            _userReferralService = userProfileService;
         }
 
         [HttpGet]
         [Route("GetFriendsList")]
         public IActionResult GetFriendList(String currentUser)
         {
-            return Ok(_userProfileService.GetUserFriendList("default"));
+            return Ok(_userReferralService.GetUserFriendList("default"));
         }
 
         [HttpPost]
         [Route("ApplyReferralCredit")]
         public IActionResult ApplyReferralCredit(string referralCode, string newUser)
         {
-            return Ok(true);
+            return Ok(_userReferralService.ApplyReferralCredit(referralCode, newUser));
         }
 
         [HttpPost]
         [Route("RecordReferralInviteSent")]
         public IActionResult RecordReferralInviteSent(string referralCode, string newUser) {
-            return Ok(true);
+            return Ok(_userReferralService.RecordReferralInviteSent(referralCode, newUser));
         }
 
         [HttpGet]
         [Route("GetReferralLink")]
         public IActionResult GetReferralLink(string referralCode)
         {
-            return Ok(_userProfileService.GetReferralLink(referralCode));
+            return Ok(_userReferralService.GetReferralLink(referralCode));
         }
     }
 }
